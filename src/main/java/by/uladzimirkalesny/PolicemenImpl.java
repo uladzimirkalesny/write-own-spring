93fe9a9a71a1512c3771a4b7312a738cfda2334a
Uladzimir Kalesny <Uladzimir_Kalesny@epam.com>
2021-10-13 13:48:51 +0300

    Create Application context, Delete ObjectFactory created IOC

--- src/main/java/by/uladzimirkalesny/PolicemenImpl.java
+++ src/main/java/by/uladzimirkalesny/PolicemenImpl.java
@@ -1,6 +1,10 @@
 package by.uladzimirkalesny;
 
 public class PolicemenImpl implements Policemen {
+
+    @InjectByType
+    private Recommender recommender;
+
     @Override
     public void makePeopleLeaveRoom() {
         System.out.println("GO AWAY");
--- src/main/java/by/uladzimirkalesny/RecommenderImpl.java
+++ src/main/java/by/uladzimirkalesny/RecommenderImpl.java
@@ -6,6 +6,10 @@ public class RecommenderImpl implements Recommender {
     @InjectProperty
     private String alcohol;
 
+    public RecommenderImpl() {
+        System.out.println("RecommenderImpl was created.");
+    }
+
     @Override
     public void recommend() {
         System.out.println("Drink " + alcohol + ". Protect yourself");
