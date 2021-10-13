980ba97d0e73f2606f6e8423b817483530235450
Uladzimir Kalesny <Uladzimir_Kalesny@epam.com>
2021-10-13 14:45:37 +0300

    added init method support

--- src/main/java/by/uladzimirkalesny/ObjectFactory.java
+++ src/main/java/by/uladzimirkalesny/ObjectFactory.java
@@ -2,6 +2,9 @@ package by.uladzimirkalesny;
 
 import lombok.SneakyThrows;
 
+import javax.annotation.PostConstruct;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,10 +29,28 @@ public class ObjectFactory {
     @SneakyThrows
     public <T> T createObject(Class<T> implClass) {
 
-        T t = implClass.getDeclaredConstructor().newInstance();
+        T t = create(implClass);
 
-        objectConfiguratorList.forEach(objectConfigurator -> objectConfigurator.configure(t, applicationContext));
+        configure(t);
+
+        invokeInit(implClass, t);
 
         return t;
+    }
+
+    private <T> T create(Class<T> implClass) throws Exception {
+        return implClass.getDeclaredConstructor().newInstance();
+    }
+
+    private <T> void configure(T t) {
+        objectConfiguratorList.forEach(objectConfigurator -> objectConfigurator.configure(t, applicationContext));
+    }
+
+    private <T> void invokeInit(Class<T> implClass, T t) throws IllegalAccessException, InvocationTargetException {
+        for (Method declaredMethod : implClass.getDeclaredMethods()) {
+            if (declaredMethod.isAnnotationPresent(PostConstruct.class)) {
+                declaredMethod.invoke(t);
+            }
+        }
     }
 }
--- src/main/java/by/uladzimirkalesny/PolicemenImpl.java
+++ src/main/java/by/uladzimirkalesny/PolicemenImpl.java
@@ -1,9 +1,16 @@
 package by.uladzimirkalesny;
 
+import javax.annotation.PostConstruct;
+
 public class PolicemenImpl implements Policemen {
 
     @InjectByType
     private Recommender recommender;
+
+    @PostConstruct
+    public void init() {
+        System.out.println(recommender.getClass().getSimpleName());
+    }
 
     @Override
     public void makePeopleLeaveRoom() {
